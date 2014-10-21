71673ac87372a2b87d969a8701fa3a8ae397af1e
grgrgrrr <grgrgrrr@grgrgrrr-P55-US3L.(none)>
2014-10-21 19:30:15 +0400

    Задание 6. До 3

--- class-06/03-coords.hs
+++ class-06/03-coords.hs
@@ -1,10 +1,39 @@
 {-
   Написать программу, которая в зависимости от параметров командной строки
-  а) генерирует случайный текстовый файл содержащий декартовы координаты точек на плоскости
-     (по одной точке в каждой строке);
-  б) определяет по заданному файлу в указанном ранее формате количество точек в каждой
-     из четвертей;
-  в) отыскивает наиболее удалённую от начала координат точку.
+
+
 -}
+import System.Environment
+import System.IO
+import System.Random
+import Data.List
+
+type Point = (Double, Double)
+
+-- а) генерирует случайный текстовый файл содержащий декартовы координаты точек на плоскости
+--   (по одной точке в каждой строке);
+randomPointsFile :: IO()
+randomPointsFile = do
+	number <- randomRIO (1,100) :: IO Int
+	genX <- getStdGen
+	genY <- getStdGen
+	writeFile "randomPointsFile.txt" (unlines $ zipWith (\a b -> show (a,b)) (newPointX number genX) (newPointY number genY))
+			where 
+				newPointX number genX = take number $ randomRs (-100, 100) genX :: [Int]
+				newPointY number genY = take number $ randomRs (-100, 100) genY :: [Int]
+
+
+-- б) определяет по заданному файлу в указанном ранее формате количество точек в каждой
+--   из четвертей;
+
+
+--к какой четверти относится точка
+findQ p 
+	| fst p > 0 && snd p > 0 = 1
+	| fst p < 0 && snd p > 0 = 2
+	| fst p < 0 && snd p < 0 = 3
+	| otherwise = 4  
+
+-- в) отыскивает наиболее удалённую от начала координат точку.
 
 main = undefined

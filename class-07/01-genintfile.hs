b3daa7f356ed304859d24ce75c90d0a9ee2aa98f
grgrgrrr <grgrgrrr@grgrgrrr-P55-US3L.(none)>
2014-10-27 22:33:50 +0400

    Задание 7. 01

--- class-07/01-genintfile.hs
+++ class-07/01-genintfile.hs
@@ -6,5 +6,24 @@
   3) количество чисел в строке;
   4) количество строк в файле.
 -}
+import System.Environment
+import System.IO
+import System.Random
+import Data.List
 
-main = undefined
+createFile  :: String -> Int -> Int -> Int -> Int -> IO ()
+createFile fname from to countNum countS = do
+	genP <- getStdGen
+	writeFile "digits.txt" (allNewDigits genP)
+			where 
+				newDigits genP = take countNum $ randomRs (from, to) (mkStdGen genP) :: [Int]
+
+				oneString genP = unwords $ map (show) $ newDigits genP
+
+				allNewDigits genP = unlines $ tail $ map(fst) $ take (countS+1) (iterate (\x -> (oneString $ snd x, snd x + 10)) ("", 1))
+
+
+
+main = do
+  [fname, from, to, countNum, countString] <- getArgs
+  createFile fname (read from) (read to) (read countNum) (read countString)
